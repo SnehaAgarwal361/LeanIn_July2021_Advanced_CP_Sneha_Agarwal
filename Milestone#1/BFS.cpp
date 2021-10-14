9fdbdc1aeda7847536f34fc689574002d7f3f5aa
Sneha Agarwal <73896596+SnehaAgarwal361@users.noreply.github.com>
2021-10-15 00:06:48 +0530

    Update BFS.cpp

--- Milestone#1/BFS.cpp
+++ Milestone#1/BFS.cpp
@@ -28,7 +28,8 @@ int main() {
     for(int i=0;i<m;i++)
     {
         cin>>u>>v;
-        adj[i].push_back(v);
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
     vector<int>vis(n,0);
     for(int i=0;i<m;i++)
